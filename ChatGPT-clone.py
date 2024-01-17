d0017d4e65990ff92ae36a74974ac59f0d6eacae
Wesley <112592508+wdam2002@users.noreply.github.com>
2024-01-16 18:08:45 -0800

    Accept user input

--- ChatGPT-clone.py
+++ ChatGPT-clone.py
@@ -14,3 +14,12 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if prompt := st.chat_input("Got a question?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
